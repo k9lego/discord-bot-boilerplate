bcc7783ad4d8bf116c96e8638793a54f2b347c76
k9lego <k9lego@users.noreply.github.com>
2020-09-16 14:41:42 -0400

    Commit Boilerplate

---
+++ src/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+if (process.env.NODE_ENV !== 'production') 
+{
+  require('dotenv').config();
+  if (!process.env.TOKEN)
+  {
+    throw new Error("ERROR: You need to create a file called .env then add \"TOKEN = (https://discord.com/developers/applications Token goes here)\"");
+  }
+}
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', () => {
+  console.log(`Bot has logged into ${client.user.tag}`);
+});
+client.login(process.env.TOKEN);
