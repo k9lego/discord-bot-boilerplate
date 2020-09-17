6feecf6af686e96bc96a978a0ba3dbb000ed1fa0
k9lego <k9lego@users.noreply.github.com>
2020-09-17 02:52:14 +0000

    Add beautify to auto-format code on save

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 'use strict';
 
-if (process.env.NODE_ENV !== 'production') 
-{
-  require('dotenv').config();
-  if (!process.env.TOKEN)
-  {
-    throw new Error("ERROR: You need to create a file called .env then add \"TOKEN = (https://discord.com/developers/applications Token goes here)\"");
-  }
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+    if (!process.env.TOKEN) {
+        throw new Error("ERROR: You need to create a file called .env then add \"TOKEN = (https://discord.com/developers/applications Token goes here)\"");
+    }
 }
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('ready', () => {
-  console.log(`Bot has logged into ${client.user.tag}`);
+    console.log(`Bot has logged into ${client.user.tag}`);
 });
 client.login(process.env.TOKEN);
