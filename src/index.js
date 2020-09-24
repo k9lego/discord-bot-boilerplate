75898b252fed01639524a6080318b128716e9657
k9lego <k9lego@users.noreply.github.com>
2020-09-24 00:36:54 +0000

    Encourage users to install local extensions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 'use strict';
 
+/*
+! You'll notice that this shows up as RED and the below line as ORANGE if you install the "Better Comments" extension.
+TODO: Install the extensions listed under "Dev Container:" at the very bottom, they are greyed out because they must be installed locally.
+*/
+
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
     if (!process.env.TOKEN) {
