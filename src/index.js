4ff05faebb35515ae32adf0606b3a6cb1a7a4fd3
k9lego <k9lego@users.noreply.github.com>
2020-09-24 01:46:35 +0000

    Commit ESLint-Standard-etc changes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,29 @@
 ! You'll notice that this shows up as RED and the below line as ORANGE if you install the "Better Comments" extension.
 TODO: Install the extensions listed under "Dev Container:" at the very bottom, they are greyed out because they must be installed locally.
 */
-
+// This is a test
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
     if (!process.env.TOKEN) {
         throw new Error("ERROR: You need to create a file called .env then add \"TOKEN = (https://discord.com/developers/applications Token goes here)\"");
     }
 }
+async function test() {
+    var test = 5;
+    if (test === 5) {
+        return true;
+    }
 
+
+
+
+
+
+
+
+    console.log(`${test}`);
+}
+test();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('ready', () => {
