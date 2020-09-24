8b6e8d545cb0c13ce73fe74877d07a4a3c271094
k9lego <k9lego@users.noreply.github.com>
2020-09-24 02:11:50 +0000

    Attempt changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 /*
 ! You'll notice that this shows up as RED and the below line as ORANGE if you install the "Better Comments" extension.
@@ -6,30 +6,23 @@ TODO: Install the extensions listed under "Dev Container:" at the very bottom, t
 */
 // This is a test
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-    if (!process.env.TOKEN) {
-        throw new Error("ERROR: You need to create a file called .env then add \"TOKEN = (https://discord.com/developers/applications Token goes here)\"");
-    }
+  require('dotenv').config();
+  if (!process.env.TOKEN) {
+    throw new Error('ERROR: You need to create a file called .env then add "TOKEN = (https://discord.com/developers/applications Token goes here)"')
+  }
 }
-async function test() {
-    var test = 5;
-    if (test === 5) {
-        return true;
-    }
+async function test () {
+  var test = 5;
+  if (test === 5) {
+    return true;
+  }
 
-
-
-
-
-
-
-
-    console.log(`${test}`);
+  console.log(`${test}`);
 }
 test();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.on('ready', () => {
-    console.log(`Bot has logged into ${client.user.tag}`);
+  console.log(`Bot has logged into ${client.user.tag}`);
 });
 client.login(process.env.TOKEN);
